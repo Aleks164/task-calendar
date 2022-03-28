3eea3961d8536fed7e3f0e5754ecbefba1790a8c
Aleks164 <alekseylehamail.ru>
2022-03-28 21:22:00 +0300

    exploring r/t

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: [
+    "airbnb-base",
+    "plugin:jest/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "import/prefer-default-export": "off",
+    "jest/valid-title": "off",
+    "no-console": "off",
+    "no-alert": "off",
+    "no-restricted-globals": "off",
+    "no-plusplus": "off",
+    "import/no-unresolved": "off",
+    "import/extensions": "off",
+    "no-useless-escape": "off",
+    "no-param-reassign": "warn",
+    "class-methods-use-this": "warn",
+    "no-new": "warn",
+    "jest/no-conditional-expect": "off",
+  },
+};
