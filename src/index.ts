42f8e6835218e21f1b828be50ad71cc72f3f5958
Aleks164 <alekseyleha@mail.ru>
2022-04-27 19:18:19 +0300

    fix index

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { calendarRender } from "./pages/calendar_page/index";
 import { aboutRender } from "./pages/about_page/index";
 import "./index.css";
 
+calendarRender();
+
 const router = Router();
 
 router.on("/task-calendar/", calendarRender);
@@ -11,5 +13,3 @@ router.on("/task-calendar/", calendarRender);
 router.on("/task-calendar/tasks", tasksMain);
 
 router.on("/task-calendar/about", aboutRender);
-
-calendarRender();
