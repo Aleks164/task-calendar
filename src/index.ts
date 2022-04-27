6202f86886c06d3932adea5aad89b8787882d4ad
Levin Aleksey <68010219+Aleks164@users.noreply.github.com>
2022-04-27 14:39:21 +0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import { calendarRender } from "./pages/calendar_page/index";
 import { aboutRender } from "./pages/about_page/index";
 import "./index.css";
 
-calendarRender();
-
 const router = Router();
 
 router.on("/task-calendar/", calendarRender);
@@ -13,3 +11,5 @@ router.on("/task-calendar/", calendarRender);
 router.on("/task-calendar/tasks", tasksMain);
 
 router.on("/task-calendar/about", aboutRender);
+
+calendarRender();
